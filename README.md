37a016c1d17389bd339a13c51aa75282b9419b16
Alessio Michelini <alessio@pond5.com>
2018-08-02 16:14:52 +0100

    Added docker-compose configuration Added sample html template

--- README.md
+++ README.md
@@ -6,11 +6,27 @@
 docker build -t flask-python-sample:latest .
 ```
 
+Or with docker-compose
+
+```
+docker-compose build
+```
+
 ## Run it
 
 ```
 docker run -d -p 5000:5000 flask-python-sample
 ```
+
+Or with docker-compose
+
+```
+docker-compose up -d
+```
+
+### Note
+
+If you use docker-compose, you can do live changes
 
 ## See it in action
 
--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Flask Dockerized'
+def main():
+    return render_template('index.html')
+
+@app.route('/hello')
+def hello():
+    return 'Hello world!'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')

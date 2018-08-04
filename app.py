b486b56781d8ccbd35f42650f67a40d37e6e6d77
Alessio Michelini <alessio@musicphotographer.eu>
2018-08-04 02:27:44 +0100

    Added small sample to how return json data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +8,12 @@ def main():
 @app.route('/hello')
 def hello():
     return 'Hello world!'
+
+@app.route('/fruits')
+def fruits():
+    list_of_fruits = ['banana', 'orange', 'apple']
+    list_of_drinks = ['coke', 'milk']
+    return jsonify(Fruits=list_of_fruits, Drinks=list_of_drinks)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
